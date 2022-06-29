0de1a6d77f1aa892eee9069079844b007dbd1d13
Tan Pham <pmtandhqn@gmail.com>
2022-06-30 04:30:15 +0700

    update the manifest and feature

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,46 +1,47 @@
 import * as React from 'react';
+import {FC, useState, useCallback} from 'react';
 import '../styles/ui.css';
 
 declare function require(path: string): any;
 
-const App = ({}) => {
-  const textbox = React.useRef<HTMLInputElement>(undefined);
+const App: FC = () => {
+  const [isLogining, setIsLogining] = useState(false);
 
-  const countRef = React.useCallback((element: HTMLInputElement) => {
-    if (element) element.value = '5';
-    textbox.current = element;
+  const handleClosePlugin = useCallback(() => {
+    parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
   }, []);
 
-  const onCreate = () => {
-    const count = parseInt(textbox.current.value, 10);
-    parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
-  };
-
-  const onCancel = () => {
-    parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
-  };
-
-  React.useEffect(() => {
-    // This is how we read messages sent from the plugin controller
-    window.onmessage = (event) => {
-      const {type, message} = event.data.pluginMessage;
-      if (type === 'create-rectangles') {
-        console.log(`Figma Says: ${message}`);
-      }
-    };
+  const handleLoginAndFetchData = useCallback(() => {
+    setIsLogining(true);
+    setTimeout(() => {
+      // simulate call external API to login Hubspot then retreive the response
+      const API_RESPONSE = [
+        {r: 1, g: 0, b: 0},
+        {r: 1, g: 1, b: 1},
+        {r: 1, g: 1, b: 0},
+      ];
+      parent.postMessage({pluginMessage: {type: 'create-color-palette', colorPalette: API_RESPONSE}}, '*');
+    }, 4000);
   }, []);
 
   return (
     <div>
-      <img src={require('../assets/logo.svg')} />
-      <h2>Rectangle Creator</h2>
-      <p>
-        Count: <input ref={countRef} />
-      </p>
-      <button id="create" onClick={onCreate}>
-        Create
-      </button>
-      <button onClick={onCancel}>Cancel</button>
+      <h2>Hubspot Logo Generator</h2>
+      {isLogining ? (
+        <div>loading...</div>
+      ) : (
+        <>
+          <span>Enter your Hubspot email and password to login</span>
+          <div className="form-container">
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Your password" />
+          </div>
+          <div className="form-button">
+            <button onClick={handleLoginAndFetchData}>Submit</button>
+            <button onClick={handleClosePlugin}>Cancel</button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,13 +1,26 @@
 figma.showUI(__html__);
 
-figma.ui.onmessage = (msg) => {
-  if (msg.type === 'create-rectangles') {
+figma.ui.onmessage = async (msg) => {
+  if (msg.type === 'create-color-palette') {
     const nodes = [];
 
-    for (let i = 0; i < msg.count; i++) {
+    // create a text
+    const text = figma.createText();
+    // Move to (50, 50)
+    text.y = -50;
+
+    // Load the font in the text node before setting the characters
+    await figma.loadFontAsync(text.fontName);
+    text.characters = 'Hubspot Color Palette';
+
+    // Set bigger font size and red color
+    text.fontSize = 64;
+    text.fills = [{type: 'SOLID', color: {r: 0, g: 0, b: 0}}];
+
+    for (let i = 0; i < msg.colorPalette.length; i++) {
       const rect = figma.createRectangle();
       rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+      rect.fills = [{type: 'SOLID', color: msg.colorPalette[i]}];
       figma.currentPage.appendChild(rect);
       nodes.push(rect);
     }
@@ -17,7 +30,7 @@ figma.ui.onmessage = (msg) => {
 
     // This is how figma responds back to the ui
     figma.ui.postMessage({
-      type: 'create-rectangles',
+      type: 'create-color-palette',
       message: `Created ${msg.count} Rectangles`,
     });
   }
